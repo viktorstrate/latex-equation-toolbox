3f9e729c199cc54c0c0ffaa37e4b83829d37f846
viktorstrate <viktorstrate@gmail.com>
2017-12-19 11:05:26 +0100

    Add mathjax

--- src/components/Mathjax/index.js
+++ src/components/Mathjax/index.js
@@ -1,9 +1,25 @@
 import { h, Component } from 'preact'
+import { connect } from 'react-redux'
+import { Context, Node } from 'react-mathjax'
 
+import style from './style.sass'
+
+@connect(store => ({
+  latex: store.input.latex
+}))
 export default class Mathjax extends Component {
-  render () {
+  render (props) {
+    let mathEl = 'Empty math'
+    if (props.latex) {
+      mathEl = <Node>{props.latex}</Node>
+    }
+
     return (
-      <div>React Component</div>
+      <div>
+        <Context>
+          {mathEl}
+        </Context>
+      </div>
     )
   }
 }
